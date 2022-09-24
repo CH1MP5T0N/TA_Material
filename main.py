2916e90d5d9e581b1036a65806cb0975acfa53ad
CH1MP5T0N <philipusadriel@gmail.com>
2022-09-25 00:11:24 +0700

    Added input

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#DOWNLOAD YOUR IDES OF CHOICE, MAKE A GITHUB ACCOUNT AND DOWNLOAD GIT!
+#These are how inputs are
+start = input("Input number: ")
+print(type(start))
+
+#Eval works like this
+start = eval(input("Input number: "))
+print(type(start))
+
+#You can make wacky stuff with input- like calculators
+quick_maths = eval(input("Input number 1:"))
+quick_maths2 = eval(input("Input number 2: "))
+print(quick_maths + quick_maths2)
+
