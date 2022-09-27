636e30106ef09699e3a51c975d1380072fa69378
CH1MP5T0N <philipusadriel@gmail.com>
2022-09-27 10:02:48 +0700

    Second TA session

---
+++ TA_week2/main.py
@@ -0,0 +1,27 @@
+# IF STATEMENTS
+choice = eval(input("Enter a number: \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n"))
+if choice == 1:
+    number1 = eval(input("Enter number1: "))
+    number2 = eval(input("Enter number2: "))
+    print(number1 + number2)
+elif choice == 2:
+    number1 = eval(input("Enter number1: "))
+    number2 = eval(input("Enter number2: "))
+    print(number1 - number2)
+elif choice == 3:
+    number1 = eval(input("Enter number1: "))
+    number2 = eval(input("Enter number2: "))
+    print(number1 * number2)
+elif choice == 4:
+    number1 = eval(input("Enter number1: "))
+    number2 = eval(input("Enter number2: "))
+    print(number1 / number2)
+else:
+    print("INVALID INPUT")
+
+x = 200
+y = 30
+z = 500
+
+if x > y or z > y:
+    print("yipee")
